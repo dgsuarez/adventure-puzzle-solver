e72a826943a2e7fd8276b4335cb3cbf3bd829262
Diego Guerra <diego.guerra.suarez@gmail.com>
2014-01-18 18:26:42 +0100

    remove template test

--- test/adventure_puzzle_solver/core_test.clj
+++ test/adventure_puzzle_solver/core_test.clj
@@ -1,7 +1,3 @@
 (ns adventure-puzzle-solver.core-test
   (:require [clojure.test :refer :all]
             [adventure-puzzle-solver.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
