820ebda33f9e13fddaec3f5ed518cec3de6d17c1
Diego Guerra <diego.guerra.suarez@gmail.com>
2014-01-25 11:40:52 +0100

    Grave Box puzzle from The Testament of Sherlock Holmes

---
+++ src/adventure_puzzle_solver/grave_box.clj
@@ -0,0 +1,41 @@
+(ns adventure-puzzle-solver.grave-box
+  (:require [clojure.set]
+            [adventure-puzzle-solver.core :refer :all]))
+
+(defn pairs-in-row [row]
+  (let [idx-pairs (map-indexed vector (map vector row (rest row)))
+        filtered (remove #(some #{0} (last %)) idx-pairs)]
+    (map first filtered)))
+
+(defn pairs [puzzle]
+  (mapcat identity (map-indexed #(map vector (repeat %1) (pairs-in-row %2)) puzzle)))
+
+(defn new-pair [pair move]
+  (vec (map + pair move)))
+
+(defn pair-cells [pair]
+  [pair [(first pair) (+ 1 (last pair))]])
+
+(defn cell-values [puzzle cells]
+  (map #(get-cell puzzle %) cells))
+
+(defn move-cells [puzzle from to]
+  (let [new-froms (map vector from '(0 0))
+        new-tos (map vector to (cell-values puzzle from))
+        new-to-set (concat new-froms new-tos)]
+    (reduce #(apply assoc-cell %1 %2) puzzle new-to-set)))
+
+(defn valid-move? [puzzle from to]
+  (let [unexplored (clojure.set/difference (set to) (set from))]
+    (and 
+      (not (some #{0} (cell-values puzzle from))) 
+      (every? #{0} (cell-values puzzle unexplored)))))
+
+(defn possible-next-steps [puzzle]
+  (remove nil? (for [move #{[-1 0] [1 0] [0 1] [0 -1]}
+                     pair (pairs puzzle)
+                     :let [from (pair-cells pair)
+                           to (pair-cells (new-pair pair move))]
+                     :when (valid-move? puzzle from to)]
+                 (move-cells puzzle from to))))
+
---
+++ test/adventure_puzzle_solver/grave_box_test.clj
@@ -0,0 +1,49 @@
+(ns adventure-puzzle-solver.grave-box-test
+  (:require [clojure.test :refer :all]
+            [adventure-puzzle-solver.grave-box :refer :all]
+            [adventure-puzzle-solver.bfser :refer :all]))
+
+(def start [[0 0 0 0 0 0 0 0 0]
+            [2 6 3 7 5 4 4 8 0]
+            [0 0 0 0 0 0 0 0 0]])
+
+(def end [[0 0 0 0 0 0 0 0 0]
+          [7 8 2 3 5 4 6 4 0]
+          [0 0 0 0 0 0 0 0 0]])
+
+(def small [[1 2 3 0]
+            [4 5 0 0]
+            [0 0 6 7]])
+
+(deftest get-the-index-of-moveable-pairs-in-a-row
+  (is (= '(0 1 4 5) (pairs-in-row [1 2 3 0 2 3 1]))))
+
+(deftest get-all-the-moveable-pairs
+  (is (= '([0 0] [0 1] [1 0] [2 2])
+         (pairs small))))
+
+(deftest get-a-new-pair-from-a-move
+  (is (= [1 1] (new-pair [1 0] [0 1]))))
+
+(deftest get-the-cells-in-a-pair
+  (is (= (list [0 0] [0 1]) (pair-cells [0 0]))))
+
+(deftest generate-a-single-next-step
+  (is (= [1 0 2 3] 
+         (get (move-cells small [[0 1] [0 2]] [[0 2] [0 3]]) 0))))
+
+(deftest know-if-a-move-is-invalid
+  (is (not (valid-move? small [[0 1] [0 2]] [[1 1] [1 2]]))))
+
+(deftest know-if-a-move-is-valid
+  (is (valid-move? small [[0 1] [0 2]] [[0 2] [0 3]])))
+
+(deftest all-get-moving
+  (is (= 5 (count (possible-next-steps small)))))
+
+(deftest all-moving-with-full
+  (is (= 15 (count (possible-next-steps start)))))
+
+;(deftest solve-it
+  ;(println (solve start #{end} possible-next-steps)))
+
