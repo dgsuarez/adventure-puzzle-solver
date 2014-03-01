abdef94e1a1cccfb1ea209179b60cd5a79969b3b
Diego Guerra <diego.guerra.suarez@gmail.com>
2014-03-01 15:22:37 +0100

    remove constant from done

--- src/adventure_puzzle_solver/seventh_cake.clj
+++ src/adventure_puzzle_solver/seventh_cake.clj
@@ -22,20 +22,20 @@
 (defn full-piece? [{:keys [spec]}]
   (every? zero? (vals spec)))
 
-(defn done? [{:keys [cake]}]
-  (every? #(>= % 3) (flatten cake)))
+(defn done? [n state]
+  (every? #(>= % n) (flatten (:cake state))))
 
 (defn update-spec [original-spec state]
   (if (full-piece? state)
     (merge state {:spec original-spec :piece-num (inc (:piece-num state))})
     state))
 
-(defn rec-split-cake [original-spec state]
-  (if (done? state) 
+(defn rec-split-cake [original-state state]
+  (if (done? (:piece-num original-state) state) 
     state
-    (some identity (map #(rec-split-cake original-spec %) 
-                        (chunks-from (update-spec original-spec state))))))
+    (some identity (map #(rec-split-cake original-state %) 
+                        (chunks-from (update-spec (:spec original-state) state))))))
 
 (defn split-cake [state]
-  (rec-split-cake (:spec state) (update-position '(0 0) state)))
+  (rec-split-cake state (update-position '(0 0) state)))
 
