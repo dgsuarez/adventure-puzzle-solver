91eb299e5a614446c62591ea06a4c9738498e7f0
Diego Guerra <diego.guerra.suarez@gmail.com>
2014-03-01 23:05:02 +0100

    new and improved backtracker

--- src/adventure_puzzle_solver/backtracker.clj
+++ src/adventure_puzzle_solver/backtracker.clj
@@ -1,15 +1,7 @@
 (ns adventure-puzzle-solver.backtracker)
 
-(defn _solve [checked state solution? get-next-states]
-  (let [new-checked (conj checked state)
-        next-states (get-next-states state)]
-    (cond 
-      (empty? next-states) nil
-      (contains? checked state) nil
-      (solution? state) (list state)
-      :else (cons state 
-                  (first (drop-while nil? (map #(_solve new-checked % solution? get-next-states) next-states)))))))
-
 (defn solve [state solution? get-next-states]
-  (_solve #{} state solution? get-next-states))
-
+  (if (solution? state)
+    [state]
+    (if-let [states (some identity (map #(solve % solution? get-next-states) (get-next-states state)))]
+      (conj states state))))
--- test/adventure_puzzle_solver/backtracker_test.clj
+++ test/adventure_puzzle_solver/backtracker_test.clj
@@ -1,26 +1,3 @@
 (ns adventure-puzzle-solver.backtracker-test
   (:require [clojure.test :refer :all]
-            [adventure-puzzle-solver.backtracker :refer :all]
-            [adventure-puzzle-solver.slider :refer :all]))
-
-
-(def one-step [[1 2 3]
-               [0 4 5]
-               [6 7 8]])
-
-(def solution [[1 2 3]
-               [4 0 5]
-               [6 7 8]])
-
-(def initial [[4 1 3]
-              [2 7 5]
-              [6 0 8]])
-
-(deftest when-is-solution
-  (solve solution #{solution} possible-next-steps))
-
-(deftest when-one-step-to-solution
- (count (solve one-step #{solution} possible-next-steps)))
-
-(deftest when-longer-steps-to-solution
- (solve initial #{solution} possible-next-steps))
+            [adventure-puzzle-solver.backtracker :refer :all]))
